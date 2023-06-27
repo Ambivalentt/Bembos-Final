1691b6ceaccfff3003b637e1cc2c7fa53f184757
Martin H <martinhj.sama@gmail.com>
2023-06-26 20:48:20 -0500

    Bembos

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {ref} from 'vue'
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import FooterView from './FooterView.vue';
+</script>
+
+<template>
+  <footer>
+    <!--FOOTER AQUI-->
+  </footer>
+</template>
+
+
+<style scoped>
+</style>
